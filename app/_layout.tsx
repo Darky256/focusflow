1148ff63f7ccb969fb6b81a07dc6b2862dd4de49
Cortina Brian James <brianjames.cortina@rpn.ch>
2024-11-21 14:08:21 +0100

    Création du stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return 
+  <Stack>
+    <Stack.Screen name="index" options={{title: 'Home'}}/>
+    <Stack.Screen name="history" options={{title: 'History'}}/>
+    <Stack.Screen name="settings" options={{title: 'Settings'}}/>
+  </Stack>;
 }
